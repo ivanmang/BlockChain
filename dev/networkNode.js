96cbc3ab412b05a6df4e4b42e9c63a938d41725e
hxm16 <hxm16@ic.ac.uk>
2018-08-05 13:06:50 +0800

    Implemented getAddress end point

--- dev/networkNode.js
+++ dev/networkNode.js
@@ -245,7 +245,11 @@ app.get('/transaction/:transactionId', function(req,res) {
 });
 
 app.get('/address/:address', function(req,res) {
-
+    const address = req.params.address;
+    const addressData = coin.getAddressData(address);
+    res.json({
+      addressData: addressData
+    });
 });
 
 app.listen(port, function() {
