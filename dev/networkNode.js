e4fb9150345dbc1f9bf5abc15606e2a0167a51dd
hxm16 <hxm16@ic.ac.uk>
2018-08-05 12:30:57 +0800

    Implemented get transaction and getHash end point

--- dev/networkNode.js
+++ dev/networkNode.js
@@ -228,11 +228,20 @@ app.get('/consensus', function(req, res) {
 });
 
 app.get('/block/:blockHash', function(req, res) {
-
+  const blockHash = req.params.blockHash;
+  const correctBlock = coin.getBlock(blockHash);
+  res.json({
+    block: correctBlock
+  });
 });
 
 app.get('/transaction/:transactionId', function(req,res) {
-
+  const transactionId = req.params.transactionId;
+  const transactionData = coin.getTransaction(transactionId);
+  res.json({
+    transaction: transactionData.transaction,
+    block: transactionData.block
+  });
 });
 
 app.get('/address/:address', function(req,res) {
